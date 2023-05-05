1c822e4371e132d0fc5773d6a38770abc93377e1
fujiwara <fujiwara.shunichiro@gmail.com>
2023-05-06 02:38:51 +0900

    add logger

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ type Bindings = {
 }
 
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+
 import {
 	MackerelWebhook,
 	newMackerelWebhook,
@@ -22,6 +24,7 @@ import {
 } from './types/mackerel'
 const app = new Hono<{ Bindings: Bindings }>()
 
+app.use('*', logger())
 app.post('/', async (c) => {
 	const hook = newMackerelWebhook(await c.req.json<MackerelWebhook>())
 	console.log(`[info] received webhook: ${JSON.stringify(hook)}`)
